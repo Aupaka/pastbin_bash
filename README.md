23ff9bcf34806741e9645da6f3b416e5411eac3f
loic lefloch <l.lefloch.91@gmail.com>
2014-11-06 03:59:58 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-pastbin_bash
-============
+# Pastebin Bash
 
 Script for send file to pastebin
+
+## Requirements
+
+A pastebin account is required.
+
+## Configuration
+
+This script need to create a configuration file: _$HOME/.pastebin.cnf_
+
+```bash
+username="pastebin_username"
+password="pastebin_password"
+api_key="pastebin_api_key"
+```
+
+The api_key can be found at http://pastebin.com/api#1
+
+## Usage
+
+Required:
+
+- f: file to send
+- p: name of the pastebin
+- t: format of the file. See [Formats availables](./formats.md)
+
+## Note
+
+This is a first version, pull requests are encouragees ! ;)
+
+## Todo
+
+- Remove a past
+- Better text display
+- Get user informations
+- Choose private option
+- Choose expire date
---
+++ pastebin.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# TODO
+# change api_paste_private
+# 0 = Public
+# 1 = Unlisted
+# 2 = Private
+#
+# change api_paste_expire_date
+# N = Never
+# 10M = 10 Minutes
+# 1H = 1 Hour
+# 1D = 1 Day
+# 1W = 1 Week
+# 2W = 2 Weeks
+# 1M = 1 Month
+
+function howto {
+    echo "Usage: [ -f file ] [ -p name ] [ -t format ] -a -h -l
+        -a -> anonymous mode
+        -h -> help
+        -l -> list"
+    exit
+}
+
+file=""
+pname=""
+format=""
+anonymous=""
+username=""
+password=""
+api_key=""
+list=""
+while getopts "f:p:t:lah" arg; do
+    case $arg in
+        f)
+            file=$OPTARG
+            ;;
+        p)
+            pname=$OPTARG
+            ;;
+        t)
+            format=$OPTARG
+            ;;
+        l)
+            list="ok"
+            ;;
+        a)
+            anonymous="ok"
+            ;;
+        h)
+            howto
+            ;;
+        *)
+            howto
+            ;;
+    esac
+done
+
+conf_filename="$HOME/.pastbin.cnf"
+
+if [ -f $conf_filename ]; then
+    echo "load config file"
+    source $conf_filename
+else
+    echo "Please create configuration file: $conf_filename"
+    exit
+fi
+
+if [[ ! -n "$username" || ! -n "$password" || ! -n "$api_key" ]]; then
+    echo "Invalid configuration file"
+    exit
+fi
+
+if [[ ! -n "$anonymous" ]]; then
+    echo "get user api key"
+    uk=$(curl -d api_dev_key="$api_key" -d api_user_name="$username" -d api_user_password="$password" http://pastebin.com/api/api_login.php)
+fi
+
+if [[ -n "$list" ]];
+then
+    curl -d api_dev_key="$api_key" -d api_user_key="$uk" -d api_user_name="$username" -d api_user_password="$password" -d api_option="list" http://pastebin.com/api/api_post.php
+else
+    # if [ -z "${s}" ] || [ -z "${p}" ];
+    if [[  ! -n "$pname" || ! -n "$format" || ! -n "$file" ]]; then
+        howto
+        exit
+    fi
+    pc=$(cat $file)
+    res=$(curl -d api_dev_key="$api_key" -d api_user_key="$uk" -d api_user_name="$username" -d api_user_password="$password" -d api_option="paste" -d api_paste_code="$pc" -d api_paste_name="$pname" -d api_paste_format="$format" http://pastebin.com/api/api_post.php)
+    echo ""
+    if [ "$res" == "Bad API request, invalid api_paste_format" ]; then
+        echo "The -t option is invalid"
+    else
+        echo $res
+    fi
+fi
+
+echo ""
+# END
